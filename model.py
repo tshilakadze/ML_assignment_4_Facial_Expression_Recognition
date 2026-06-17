21ebaf0a364f2a1ca43ff47f0d24378b8cac2a67
tshilakadze <tshil23@freeuni.edu.ge>
2026-06-17 15:35:16 +0400

    next architecture, MediumCNN

--- model.py
+++ model.py
@@ -51,3 +51,36 @@ class SmallCNNRegularized(nn.Module):
         x = self.features(x)
         x = self.classifier(x)
         return x
+
+
+class MediumCNN(nn.Module):
+    def __init__(self, num_classes=7, dropout_p=0.3):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        self.global_pool = nn.AdaptiveAvgPool2d(1)
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(dropout_p),
+            nn.Linear(128, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.global_pool(x)
+        x = self.classifier(x)
+        return x
